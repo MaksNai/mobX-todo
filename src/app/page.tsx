52085c299c8b404abbf87804b4331486d3f5bac3
Max <maks.reacter@gmail.com>
2024-09-30 14:34:56 +0300

    added mobx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,80 @@
-import Image from "next/image";
+"use client";
+
+import { todoStore } from "@/libs/mobx";
+import { observer } from "mobx-react-lite";
+import { useCallback, useState } from "react";
 import styles from "./page.module.css";
 
-export default function Home() {
+function Home() {
+  const [newTask, setNewTask] = useState<string>("");
+
+  const handleAddToDo = useCallback(() => {
+    todoStore.addTodo(newTask);
+    setNewTask("");
+  }, [newTask]);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+        <div className={styles.container}>
+          <div className={styles.navTask}>
+            <h3 className={styles.navHeader}>Set new task</h3>
+            <input
+              type="text"
+              className={styles.inputTask}
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <button className={styles.buttonAddTask} onClick={handleAddToDo}>
+              Add task
+            </button>
+          </div>
+          <div className={styles.taskList}>
+            {todoStore.todos.length > 0 ? (
+              todoStore.todos.map((todo, index) => (
+                <div
+                  key={index}
+                  className={`${styles.taskItem} ${
+                    todo.isImportant ? styles.important : ""
+                  }`}
+                >
+                  <span
+                    className={`${styles.taskText} ${
+                      todo.isDone ? styles.completed : ""
+                    }`}
+                  >
+                    {todo.task}
+                  </span>
+                  <div className={styles.taskActions}>
+                    <button
+                      className={styles.actionButton}
+                      onClick={() => todoStore.toggleDoneTodo(index)}
+                    >
+                      {todo.isDone ? "Undo" : "Done"}
+                    </button>
+                    <button
+                      className={styles.actionButton}
+                      onClick={() => todoStore.toggleImportantTodo(index)}
+                    >
+                      {todo.isImportant ? "Unmark" : "Important"}
+                    </button>
+                    <button
+                      className={styles.deleteButton}
+                      onClick={() => todoStore.deleteTodo(index)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>No tasks available</p>
+            )}
+          </div>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
+
+export default observer(Home);
